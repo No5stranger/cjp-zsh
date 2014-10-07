bdd568767d64090ac677a0387f3a1119de4be665
cjp <No5stranger@gmail.com>
2014-10-07 22:59:54 +0800

    personal zsh configure init

---
+++ custom/cjp.zsh
@@ -0,0 +1,45 @@
+#cjp zsh config
+
+#alias
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/bin
+
+alias ll='ls -l'
+alias la='ls -a'
+alias tmux='tmux -2'
+
+#bind key
+export EIDTOR=vim
+bindkey jk vi-cmd-mode
+
+#man plugin configure
+function man() {
+      env \
+          LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+          LESS_TERMCAP_md=$(printf "\e[1;31m") \
+          LESS_TERMCAP_me=$(printf "\e[0m") \
+          LESS_TERMCAP_se=$(printf "\e[0m") \
+          LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+          LESS_TERMCAP_ue=$(printf "\e[0m") \
+          LESS_TERMCAP_us=$(printf "\e[1;32m") \
+               man "$@"
+}
+
+#autojump plugin configure
+if [ $commands[autojump] ]; then # check if autojump is installed
+  if [ -f $HOME/.autojump/etc/profile.d/autojump.zsh ]; then # manual user-local installation
+    . $HOME/.autojump/etc/profile.d/autojump.zsh
+  elif [ -f /usr/share/autojump/autojump.zsh ]; then # debian and ubuntu package
+    . /usr/share/autojump/autojump.zsh
+  elif [ -f /etc/profile.d/autojump.zsh ]; then # manual installation
+    . /etc/profile.d/autojump.zsh
+  elif [ -f /etc/profile.d/autojump.sh ]; then # gentoo installation
+    . /etc/profile.d/autojump.sh
+  elif [ -f /usr/local/share/autojump/autojump.zsh ]; then # freebsd installation
+    . /usr/local/share/autojump/autojump.zsh
+  elif [ -f /opt/local/etc/profile.d/autojump.zsh ]; then # mac os x with ports
+    . /opt/local/etc/profile.d/autojump.zsh
+  elif [ $commands[brew] -a -f `brew --prefix`/etc/autojump.zsh ]; then # mac os x with brew
+    . `brew --prefix`/etc/autojump.zsh
+  fi
+fi
+
---
+++ zshrc
@@ -0,0 +1,16 @@
+ZSH=$HOME/.zsh/oh-my-zsh
+
+ZSH_THEME="robbyrussell"
+
+ZSH_CUSTOM=$HOME/.zsh/custom
+
+plugins=(
+    vi-mode
+    colored-man
+    git
+    github
+    composer
+    vagrant
+)
+
+source $ZSH/oh-my-zsh.sh
